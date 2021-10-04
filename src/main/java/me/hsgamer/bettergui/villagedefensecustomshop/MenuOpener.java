52e433fe0213c5999983a355d2e9303abc43aa44
Huynh Tien <huynhqtienvtag@gmail.com>
2021-10-04 08:03:45 +0700

    open from menu manager

--- src/main/java/me/hsgamer/bettergui/villagedefensecustomshop/MenuOpener.java
+++ src/main/java/me/hsgamer/bettergui/villagedefensecustomshop/MenuOpener.java
@@ -1,6 +1,6 @@
 package me.hsgamer.bettergui.villagedefensecustomshop;
 
-import me.hsgamer.bettergui.api.menu.Menu;
+import me.hsgamer.bettergui.BetterGUI;
 import org.bukkit.entity.Player;
 import plugily.projects.villagedefense.arena.Arena;
 import plugily.projects.villagedefense.arena.ArenaRegistry;
@@ -11,14 +11,14 @@ import java.util.TreeMap;
 import java.util.function.Consumer;
 
 public class MenuOpener implements Consumer<Player> {
-    private final TreeMap<Integer, Menu> map = new TreeMap<>();
+    private final TreeMap<Integer, String> map = new TreeMap<>();
     private final Arena arena;
 
     public MenuOpener(Arena arena) {
         this.arena = arena;
     }
 
-    public TreeMap<Integer, Menu> getMap() {
+    public TreeMap<Integer, String> getMap() {
         return map;
     }
 
@@ -30,6 +30,6 @@ public class MenuOpener implements Consumer<Player> {
         int wave = arena.getWave();
         Optional.ofNullable(map.floorEntry(wave))
                 .map(Map.Entry::getValue)
-                .ifPresent(menu -> menu.createInventory(player, new String[0], true));
+                .ifPresent(menu -> BetterGUI.getInstance().getMenuManager().openMenu(menu, player, new String[0], true));
     }
 }
